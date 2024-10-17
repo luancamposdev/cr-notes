27d263c4ffd597fe32e53721c999f7f6eb1aaed8
Luan Campos <luancamposdeveloper@gmail.com>
2024-10-17 11:05:37 -0300

    chore: create delete button action

---
+++ src/renderer/src/components/Button/DeleteNoteButton.tsx
@@ -0,0 +1,11 @@
+import { FaRegTrashCan } from 'react-icons/fa6'
+
+import { ActionButton, type ActionButtonProps } from '@/components'
+
+export const DeleteNoteButton = ({ ...props }: ActionButtonProps) => {
+  return (
+    <ActionButton {...props}>
+      <FaRegTrashCan className="w-4 h-4 text-zinc-300" />
+    </ActionButton>
+  )
+}
