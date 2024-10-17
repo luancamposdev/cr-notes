88b5c5126b096b137004aedf321ef50b9853f041
Luan Campos <luancamposdeveloper@gmail.com>
2024-10-17 11:09:01 -0300

    chore: create mock Note info

---
+++ src/renderer/src/store/mocks/index.ts
@@ -0,0 +1,20 @@
+import { NoteInfo } from '@shared/models'
+
+export const notesMock: NoteInfo[] = [
+  {
+    title: 'Welcome ✋🏻',
+    lastEditTime: new Date().getTime()
+  },
+  {
+    title: 'Lista Despesas 💵',
+    lastEditTime: new Date().getTime()
+  },
+  {
+    title: 'My todo 📚',
+    lastEditTime: new Date().getTime()
+  },
+  {
+    title: 'my tasks 🗞',
+    lastEditTime: new Date().getTime()
+  }
+]
