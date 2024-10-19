e2d909789b28ee641b64d75f51d4886acff58253
Luan Campos <luancamposdeveloper@gmail.com>
2024-10-19 10:09:42 -0300

    chore: create function for action button

--- src/renderer/src/components/Button/DeleteNoteButton.tsx
+++ src/renderer/src/components/Button/DeleteNoteButton.tsx
@@ -1,10 +1,17 @@
 import { FaRegTrashCan } from 'react-icons/fa6'
+import { useSetAtom } from 'jotai'
 
 import { ActionButton, type ActionButtonProps } from '@/components'
+import { deleteNoteAtom } from '@/store'
 
 export const DeleteNoteButton = ({ ...props }: ActionButtonProps) => {
+  const deleteNote = useSetAtom(deleteNoteAtom)
+
+  const handleDeleteNote = () => {
+    deleteNote()
+  }
   return (
-    <ActionButton {...props}>
+    <ActionButton {...props} onClick={handleDeleteNote}>
       <FaRegTrashCan className="w-4 h-4 text-zinc-300" />
     </ActionButton>
   )
--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,10 +1,18 @@
 import { LuFileSignature } from 'react-icons/lu'
+import { useSetAtom } from 'jotai'
 
 import { ActionButton, type ActionButtonProps } from '@/components'
+import { createEmptyNoteAtom } from '@/store'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom)
+
+  const handleCreate = () => {
+    createEmptyNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton {...props} onClick={handleCreate}>
       <LuFileSignature className="w-4 h-4 text-zinc-300" />
     </ActionButton>
   )
